959a9e1051a294cf5cf147f6772f4d7f1289a334
김다예 <wow_da65@naver.com>
2022-11-19 15:57:25 +0900

    트릭2: 모든 태그에 동일한 속성값을 넣을 필요가 없다! .attrs를 이용해서 한번만 작성해주면 계속 사용가능!

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import styled from "styled-components";
 
 const Father = styled.div``;
 
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="https://naver.com">
-        NAVER
-      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
