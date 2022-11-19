d17a4d5fbea130779c906ad23e19393bfd47f7e2
김다예 <wow_da65@naver.com>
2022-11-19 12:31:16 +0900

    리액트 기본 설정 (버전 17)

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">기본틀</div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import ReactDOM from 'react-dom/client'; // react v18 버전용
+import ReactDOM from "react-dom"; // react v17 버전용
+import App from "./App";
+
+// react v 18 버전 용
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//         <App />
+//   </React.StrictMode>
+// );
+
+// react v 17 버전용
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
