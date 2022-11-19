d780f76857e87343daf96db3bc7fd7db98b164c1
김다예 <wow_da65@naver.com>
2022-11-19 15:49:43 +0900

    트릭1: 이미 태그를 지정해둔걸 다른 태그로 쓰고 싶을땐 as라는 속성값으로 변경하고 싶은 태그를 적어준다. GOOD!

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import styled from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
+const Father = styled.div``;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Text = styled.span`
-  font-size: 20px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal">
-        <Text>Hi</Text>
-      </Box>
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="https://naver.com">
+        NAVER
+      </Btn>
     </Father>
   );
 }
